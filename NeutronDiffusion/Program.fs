18c1e2845873a32d09e41cc2008e3672aeba9fe2
Jackson Fellows <jackson@fellowsfamily.com>
2019-05-04 17:48:48 -0700

    week 4

--- NeutronDiffusion/Program.fs
+++ NeutronDiffusion/Program.fs
@@ -95,6 +95,129 @@ let week3 () =
     File.OpenWrite mat_lib_name |> mat_file.WriteTo
     File.OpenWrite "output.obj" |> model.WriteTo
 
+open System.Numerics
+open System.Drawing
+open System
+
+type Texture =
+    { bitmap:Bitmap
+      width:int
+      height:int
+      get_coords:int -> Vector2*Vector2*Vector2 }
+    interface IDisposable with
+        member this.Dispose () = this.bitmap.Dispose ()
+
+let set_tex_coord (texture : Texture) (uv : Vector2) color =
+    texture.bitmap.SetPixel (uv.X*(float32 (texture.width-1)) |> int, (1.0f - uv.Y)*(float32 (texture.height-1)) |> int, color)
+
+let add_texture (model : WavefrontObj.ObjFile) (r_width,r_height) cols : Texture =
+    let num_tris = model.Faces.Count
+    let num_rects = num_tris / 2
+    // TODO: how should I choose to setup my grid?
+    let rows = float num_rects / float cols |> ceil |> int
+    let tex_width = r_width * cols
+    let tex_height = r_height * rows
+
+    let tex_coords = [|
+        for row in 0..rows do
+            for col in 0..cols do
+                let u = float32 (col*r_width) / float32 tex_width
+                let v = 1.0f - float32 (row*r_height) / float32 tex_height
+                yield Vector2(u,v)
+    |]
+
+    let get_tex_coord_indices tri_i =
+        let (rect_i,o) = Math.DivRem (tri_i,2)
+        let (row,col) = Math.DivRem (rect_i,cols)
+        let at (r,c) = (cols+1) * r + c
+        let ia = at (row,col)
+        let ib = if o = 0 then at (row+1,col) else at (row+1,col+1)
+        let ic = if o = 0 then at (row+1,col+1) else at (row,col+1)
+        // TODO: remove checks if they are slowing things down
+        if ia = ib || ia = ic || ib = ic then
+            failwith "texture indices are the same"
+        if ia < 0 || ib < 0 || ic < 0 then
+            failwith "texture indices are negative"
+        (ia,ib,ic)
+
+    let get_tex_coords tri_i =
+        let (ia,ib,ic) = get_tex_coord_indices tri_i
+        (tex_coords.[ia], tex_coords.[ib], tex_coords.[ic])
+
+    let texture : Bitmap = new Bitmap(tex_width, tex_height)
+
+    printfn "Num tris: %d" num_tris
+    printfn "Rows: %d" rows
+    printfn "Cols: %d" cols
+
+    Seq.iter (fun (tex_coord : Vector2) ->
+        model.TextureVertices.Add(WavefrontObj.ObjVector3(tex_coord.X,tex_coord.Y))
+    ) tex_coords
+
+    // set up the texture coords
+    Seq.iteri (fun tri_i (face : WavefrontObj.ObjFace) ->
+        let (ia,ib,ic) = get_tex_coord_indices tri_i
+        let set_texture vi ti =
+            let mutable triplet = face.Vertices.[vi]
+            triplet.Texture <- ti+1
+            face.Vertices.[vi] <- triplet
+        set_texture 0 ia
+        set_texture 1 ib
+        set_texture 2 ic
+    ) model.Faces
+
+    { bitmap=texture; width=tex_width; height=tex_height; get_coords=get_tex_coords }
+
+let get_uv_coords ((va,vb,vc) : Vector3*Vector3*Vector3) ((ta,tb,tc) : Vector2*Vector2*Vector2) (point : Vector3) : Vector2 =
+    if va = vb || va = vc || vb = vc then
+        failwith "triangle vertices are the same!"
+
+    // vectors from point to vertices
+    let fa = va - point
+    let fb = vb - point
+    let fc = vc - point
+    // areas and factors
+    let a = Vector3.Cross(va-vb, va-vc).Length() // main area
+    let a1 = Vector3.Cross(fb, fc).Length() / a
+    let a2 = Vector3.Cross(fc, fa).Length() / a
+    let a3 = Vector3.Cross(fa, fb).Length() / a
+    // get final uv coord
+    ta * a1 + tb * a2 + tc * a3
+
+let week4 () =
+    let model = File.OpenRead "baseparaffinblock.stl"
+              |> StereoLithography.STLDocument.Read
+              |> stl_file_to_obj
+
+    let vertices = [| for vertex in model.Vertices do yield Vector3(vertex.Position.X,vertex.Position.Y,vertex.Position.Z) |]
+    let texture_file_name = "texture.bmp"
+    let mat_lib_name = "week4.mtl"
+    let mat_name = "textured_material"
+
+    using (add_texture model (100,100) 20) (fun texture ->
+        // mark the center of each face
+        Seq.iteri (fun tri_i (face : WavefrontObj.ObjFace) ->
+            let (ta,tb,tc) as tex_coords = texture.get_coords tri_i
+            let [va;vb;vc] = List.init 3 (fun i -> vertices.[face.Vertices.[i].Vertex-1])
+            let tri = (va,vb,vc)
+
+            let center = (va+vb+vc) / 3.0f
+            set_tex_coord texture (get_uv_coords tri tex_coords center) Color.White
+
+            set_tex_coord texture ta Color.Red
+            set_tex_coord texture tb Color.Red
+            set_tex_coord texture tc Color.Red
+        ) model.Faces
+
+        // In Haskell, I would curry and flip texture.bitmap.Save
+        File.OpenWrite texture_file_name |> fun stream -> texture.bitmap.Save (stream,Imaging.ImageFormat.Bmp)
+    )
+
+    let mat_file = add_texture_to_model model mat_lib_name texture_file_name mat_name
+
+    File.OpenWrite mat_lib_name |> mat_file.WriteTo
+    File.OpenWrite "week4.obj" |> model.WriteTo
+
 [<EntryPoint>]
 let main argv =
     0 // return an integer exit code
