357364716b36558f1a64c48016ec337d0f526203
Jackson Fellows <jackson@fellowsfamily.com>
2019-06-12 10:50:37 -0700

    moved STL stuff to seperate file

--- misc/build_scene.py
+++ misc/build_scene.py
@@ -1,61 +1,20 @@
 import json
-import sys
 import os.path
+import sys
 
-try:
-    scene_filename = sys.argv[1]
-    with open(scene_filename) as f:
-        scene_file = json.load(f)
-except IndexError:
-    raise ValueError('no file name given')
+from STL import STL
 
-stls = {}
+scene_filename = sys.argv[1]
+scene_basename = os.path.basename(scene_filename)
+scene_dir = os.path.dirname(scene_filename)
+scene_pairs = []
+with open(scene_filename) as scene_file:
+    scene = json.load(scene_file)
+    for obj in scene:
+        stl = STL(os.path.join(scene_dir, obj['file']))
+        stl.vertices += obj['offset']
+        scene_pairs.extend(zip(stl.normals,stl.facets))
 
-def read_stl(path):
-    if path in stls:
-        return stls[path]
-    (normals,facets) = [],[]
-    with open(os.path.join(os.path.dirname(scene_filename),path)) as stl_file:
-        stl_lines = stl_file.readlines()
-        for i in range(1,len(stl_lines)-1,7):
-            facet = stl_lines[i:i+7]
-            (normal,v1,v2,v3) = [ list(map(float,facet[i].split()[-3:])) for i in [0,2,3,4] ]
-            normals.append(normal)
-            facets.append([v1,v2,v3])
-    stl = (normals,facets)
-    stls[path] = stl
-    return stl
+scene_stl = STL(pair=zip(*scene_pairs))
+scene_stl.write(f'{os.path.splitext(scene_basename)[0]}.stl')
 
-def show_v3(v):
-    # TODO: use scientific notation?
-    return ' '.join(map(str,v))
-
-def indent(strs):
-    return [ '\t'+s for s in strs ]
-
-def write_stl(stl,name,path):
-    with open(path, 'w') as stl_file:
-        stl_file.write(f'solid "{name}"\n')
-        for (normal,facet) in stl:
-            stl_file.write(
-                '\n'.join(indent(
-                    [ f'facet normal {show_v3(normal)}']
-                    + indent(
-                        ['outer loop']
-                        + indent([ f'vertex {show_v3(vertex)}' for vertex in facet ]) +
-                        ['endloop']
-                    ) +
-                    [f'endfacet']
-                )) + '\n'
-            )
-        stl_file.write(f'endsolid "{name}"\n')
-
-scene_stl = []
-
-for block in scene_file:
-    (normals,facets) = read_stl(block['file'])
-    offset = list(map(float,block['offset']))
-    new_facets = [ [ [ v+o for (v,o) in zip(vertex,offset) ] for vertex in facet] for facet in facets ]
-    scene_stl.extend(zip(normals,new_facets))
-
-write_stl(scene_stl, scene_filename, os.path.splitext(os.path.basename(scene_filename))[0] + '.stl')
